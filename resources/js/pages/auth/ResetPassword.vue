a76a68ea642c515567e4a878a06b8819a5f9ae99
Dom Elves <dom_elves@hotmail.co.uk>
2026-08-18 11:03:23 +0100

    check import order

--- resources/js/pages/auth/ResetPassword.vue
+++ resources/js/pages/auth/ResetPassword.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { Form } from '@inertiajs/vue3';
 import { onMounted } from 'vue';
-import GuestLayout from '@/layouts/GuestLayout.vue';
 import TextInput from '@/components/forms/TextInput.vue';
+import GuestLayout from '@/layouts/GuestLayout.vue';
 import InputLabel from '@/components/forms/InputLabel.vue';
 
 const props = defineProps<{
