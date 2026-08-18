0a7407c2bacf2c2780e65ab9746391d5dca61fd5
Dom Elves <dom_elves@hotmail.co.uk>
2026-08-18 22:21:40 +0100

    modal stolen from chopr, add to main dash

---
+++ resources/js/components/Modal.vue
@@ -0,0 +1,123 @@
+<script setup lang="ts">
+import { computed, onMounted, onUnmounted, ref, watch } from 'vue';
+
+const emit = defineEmits(["close"]);
+
+const props = defineProps({
+    show: {
+        type: Boolean,
+        default: false,
+    },
+    maxWidth: {
+        type: String,
+        default: '2xl',
+    },
+    closeable: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+const dialog = ref();
+const showSlot = ref(props.show);
+
+watch(
+    () => props.show,
+    () => {
+        if (props.show) {
+            document.body.style.overflow = 'hidden';
+            showSlot.value = true;
+
+            dialog.value?.showModal();
+        } else {
+            document.body.style.overflow = '';
+
+            setTimeout(() => {
+                dialog.value?.close();
+                showSlot.value = false;
+            }, 200);
+        }
+    },
+);
+
+const close = () => {
+    if (props.closeable) {
+        emit('close');
+    }
+};
+
+const closeOnEscape = (e) => {
+    if (e.key === 'Escape') {
+        e.preventDefault();
+
+        if (props.show) {
+            close();
+        }
+    }
+};
+
+onMounted(() => document.addEventListener('keydown', closeOnEscape));
+
+onUnmounted(() => {
+    document.removeEventListener('keydown', closeOnEscape);
+
+    document.body.style.overflow = '';
+});
+
+const maxWidthClass = computed(() => {
+    return {
+        sm: 'sm:max-w-sm',
+        md: 'sm:max-w-md',
+        lg: 'sm:max-w-lg',
+        xl: 'sm:max-w-xl',
+        '2xl': 'sm:max-w-2xl',
+    }[props.maxWidth];
+});
+</script>
+
+<template>
+    <dialog
+    ref="dialog"
+    class="z-50 m-0 min-h-full min-w-full overflow-y-auto bg-transparent p-0 backdrop:bg-transparent"
+>
+    <div class="fixed inset-0 z-50 overflow-y-auto px-4 py-6 sm:px-0">
+
+        <!-- Overlay -->
+        <Transition
+            enter-active-class="ease-out duration-300"
+            enter-from-class="opacity-0"
+            enter-to-class="opacity-100"
+            leave-active-class="ease-in duration-200"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
+        >
+            <div
+                v-show="show"
+                class="fixed inset-0 z-0"
+                @click="close"
+            >
+                <div class="absolute inset-0 bg-gray-500/75" />
+            </div>
+        </Transition>
+
+        <!-- Content -->
+        <Transition
+            enter-active-class="ease-out duration-300"
+            enter-from-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enter-to-class="opacity-100 translate-y-0 sm:scale-100"
+            leave-active-class="ease-in duration-200"
+            leave-from-class="opacity-100 translate-y-0 sm:scale-100"
+            leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        >
+            <div
+                v-show="show"
+                class="relative z-10 mb-6 overflow-hidden rounded-lg bg-white shadow-xl sm:mx-auto sm:w-full"
+                :class="maxWidthClass"
+            >
+                <slot v-if="showSlot" />
+            </div>
+        </Transition>
+
+    </div>
+</dialog>
+</template>
--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
-import { usePage, Link } from '@inertiajs/vue3';
-import { onMounted } from 'vue';
+import { usePage } from '@inertiajs/vue3';
+import { onMounted, ref } from 'vue';
 import AuthenticatedLayout from '@/layouts/AuthenticatedLayout.vue';
+import Modal from '@/components/Modal.vue';
 
 const page = usePage();
-
+const showModal = ref(false)
 const user = page.props.auth.user;
+const creatingGame = ref(false);
+const joiningGame = ref(false);
+
+function closeModal() {
+    showModal.value = false;
+    creatingGame.value = false;
+    joiningGame.value = false;
+}
 
 onMounted(() => {
     console.log(user);
@@ -16,18 +25,22 @@ onMounted(() => {
     <AuthenticatedLayout>
         <div class="m-4">gz auth {{ user.name }}</div>
         <div>
-            <Link
-                href="/create"
+            <button
+                @click="showModal = true; creatingGame = true"
                 class="button mx-2 rounded border-1 border-[#1b1b18] bg-transparent p-2 text-[#1b1b18] hover:bg-[#1b1b18] hover:text-white dark:border-[#EDEDEC] dark:text-[#EDEDEC] dark:hover:bg-[#EDEDEC] dark:hover:text-[#1b1b18]"
             >
                 Create
-            </Link>
-            <Link
-                href="/join"
+            </button>
+            <button
+                @click="showModal = true; joiningGame = true"
                 class="button mx-2 rounded border-1 border-[#1b1b18] bg-transparent p-2 text-[#1b1b18] hover:bg-[#1b1b18] hover:text-white dark:border-[#EDEDEC] dark:text-[#EDEDEC] dark:hover:bg-[#EDEDEC] dark:hover:text-[#1b1b18]"
             >
                 Join
-            </Link>
+            </button>
         </div>
+        <Modal :show="showModal" @close="closeModal">
+            <p v-if="creatingGame">Creating..</p>
+            <p v-if="joiningGame">Joining..</p>
+        </Modal>
     </AuthenticatedLayout>
 </template>
