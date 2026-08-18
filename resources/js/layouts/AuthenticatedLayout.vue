55ea7a98b8bb13ac66703f592dc0a77dbda2ac9e
Dom Elves <dom_elves@hotmail.co.uk>
2026-08-18 14:16:07 +0100

    move logout to auth layout

--- resources/js/layouts/AuthenticatedLayout.vue
+++ resources/js/layouts/AuthenticatedLayout.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-
-import { usePage } from '@inertiajs/vue3';
+import { usePage, router } from '@inertiajs/vue3';
 import { onMounted } from 'vue';
 
 const page = usePage();
 const user = page.props.auth.user;
+
+const logout = () => {
+    router.post('/logout')
+}
 
 onMounted(() => {
     console.log('auth user', user);
@@ -17,6 +20,13 @@ onMounted(() => {
             <ul>
                 <li>auth layout</li>
                 <li><a href="/dashboard">dashboard</a></li>
+                <li>
+                    <button
+                        @click="logout"
+                        class="button mx-2 p-2 border-1 rounded border-[#1b1b18] bg-transparent text-[#1b1b18] hover:bg-[#1b1b18] hover:text-white dark:border-[#EDEDEC] dark:text-[#EDEDEC] dark:hover:bg-[#EDEDEC] dark:hover:text-[#1b1b18]"
+                    >
+                        Log Out
+                    </button></li>
             </ul>
         </nav>
         <main class="flex flex-col items-center justify-center w-full h-screen bg-red-100">
--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
-import { usePage, router } from '@inertiajs/vue3';
+import { usePage } from '@inertiajs/vue3';
 import { onMounted } from 'vue';
 import AuthenticatedLayout from '@/layouts/AuthenticatedLayout.vue';
 
 const page = usePage();
 
 const user = page.props.auth.user;
-
-const logout = () => {
-    router.post('/logout')
-}
 
 onMounted(() => {
     console.log(user);
@@ -22,11 +18,5 @@ onMounted(() => {
         <div>
             gz auth {{ user.name }}
         </div>
-        <button
-            @click="logout"
-            class="button mx-2 p-2 border-1 rounded border-[#1b1b18] bg-transparent text-[#1b1b18] hover:bg-[#1b1b18] hover:text-white dark:border-[#EDEDEC] dark:text-[#EDEDEC] dark:hover:bg-[#EDEDEC] dark:hover:text-[#1b1b18]"
-        >
-            Log Out
-        </button>
     </AuthenticatedLayout>
 </template>
