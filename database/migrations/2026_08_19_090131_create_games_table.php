0ce60ab4c16f61d19a405c7d4d87c6478b5dbfaa
Dom Elves <dom_elves@hotmail.co.uk>
2026-08-19 14:35:10 +0100

    add name to game table

--- database/migrations/2026_08_19_090131_create_games_table.php
+++ database/migrations/2026_08_19_090131_create_games_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
             $table->string('password')->nullable();
             $table->integer('hands')->nullable();
             $table->boolean('finished')->default(false);
