06ecc450c0257c1a5021190e1e58795b86090cbe
Dom Elves <dom_elves@hotmail.co.uk>
2026-08-19 14:36:00 +0100

    game controller, incomplete

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Game;
+use App\Models\InviteLink;
+use Illuminate\Support\Str;
+use Inertia\Inertia;
+
+class GameController extends Controller
+{
+    public function create(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'nullable|string|max:255',
+            'password' => 'nullable|string|max:255',
+        ]);
+
+        $game = Game::create([
+            'name' => $validated['name'],
+            'password' => $validated['password'],
+        ]);
+
+        $inviteLink = InviteLink::create([
+            'game_id' => $game->id,
+            'user_id' => auth()->user()->id,
+            'token' => Str::random(8),
+        ]);
+
+        return Inertia::render('Game', [
+            'game' => $game,
+            'inviteLink' => $inviteLink,
+        ]);
+    }
+}
