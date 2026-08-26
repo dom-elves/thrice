7ad1f1cad6e00d5650ed6de990f075c793d247de
Dom Elves <dom_elves@hotmail.co.uk>
2026-08-26 15:55:33 +0100

    test for user joining game

--- tests/Feature/Game/GameTest.php
+++ tests/Feature/Game/GameTest.php
@@ -4,6 +4,7 @@ use App\Events\GameUserCreated;
 use App\Models\Game;
 use App\Models\User;
 use Illuminate\Support\Facades\Event;
+use Inertia\Testing\AssertableInertia as Assert;
 
 beforeEach( function () {
     $this->user = User::factory()->create();
@@ -34,3 +35,43 @@ test('user can create a game, and a game user for them is created', function() {
         'game_id' => $game->id,
     ]);
 });
+
+test('user can join a created game, and has a user created for them', function() {
+    $game = Game::factory()->create();
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->get(route('game.show', $game));
+
+    Event::assertDispatched(GameUserCreated::class);
+
+    $response->assertInertia(fn (Assert $page) =>
+        $page->component('Game')
+            ->has('game')
+            ->where('game.id', $game->id)
+    );
+
+    $this->assertDatabaseHas('games', [
+        'name' => $game->name,
+        'password' => $game->password,
+    ]);
+
+    $this->assertDatabaseHas('game_users', [
+        'user_id' => $user->id,
+        'game_id' => $game->id,
+    ]);
+});
+
+test('user can not join a game that does not exist', function() {
+
+});
+
+test('user can not join a game that has finished', function() {
+
+});
+
+test('user can not join a game that is full', function() {
+
+});
+
+// todo: tests for invites etc, when that is built
+// as well as looking into how to test redis
