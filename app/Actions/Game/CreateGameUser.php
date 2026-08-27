91cd6c48f667407c4586085a4cad22878e42fa03
Dom Elves <dom_elves@hotmail.co.uk>
2026-08-27 14:53:33 +0100

    spacing

--- app/Actions/Game/CreateGameUser.php
+++ app/Actions/Game/CreateGameUser.php
@@ -25,13 +25,13 @@ class CreateGameUser
             event(new GameUserCreated($gameUser));
 
             DB::afterCommit(function () use ($gameUser) {
-                Redis::pipeline(function($pipe) use ($gameUser) {
-                    $pipe->hset("game_user:{$gameUser->id}", "game_id", $gameUser->game->id);
-                    $pipe->hset("game_user:{$gameUser->id}", "user_id", $user->id);
-                    $pipe->hset("game_user:{$gameUser->id}", "start_balance", $gameUser->start_balance);
-                    $pipe->hset("game_user:{$gameUser->id}", "end_balance", $gameUser->end_balance);
-                    $pipe->hset("game_user:{$gameUser->id}", "join_time", Carbon::now()->toDateTimeString());
-                    $pipe->hset("game_user:{$gameUser->id}", "leave_time", null);
+                Redis::pipeline(function ($pipe) use ($gameUser) {
+                    $pipe->hset("game_user:{$gameUser->id}", 'game_id', $gameUser->game->id);
+                    $pipe->hset("game_user:{$gameUser->id}", 'user_id', $user->id);
+                    $pipe->hset("game_user:{$gameUser->id}", 'start_balance', $gameUser->start_balance);
+                    $pipe->hset("game_user:{$gameUser->id}", 'end_balance', $gameUser->end_balance);
+                    $pipe->hset("game_user:{$gameUser->id}", 'join_time', Carbon::now()->toDateTimeString());
+                    $pipe->hset("game_user:{$gameUser->id}", 'leave_time', null);
                 });
             });
 
