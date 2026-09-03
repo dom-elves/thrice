ab0425cbe4fb3f5d011a842a6605deb8d85364a7
Dom Elves <dom_elves@hotmail.co.uk>
2026-09-03 22:39:02 +0100

    move game full check to status middleware

--- app/Http/Middleware/CheckGameStatus.php
+++ app/Http/Middleware/CheckGameStatus.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Models\Game;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 use Inertia\Inertia;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -24,6 +25,14 @@ class CheckGameStatus
         if ($game->finished) {
             Inertia::flash([
                 'message' => 'Game is finished',
+            ]);
+
+            return redirect('dashboard');
+        }
+
+        if (Redis::scard("game:{$game->id}:game_user_ids") === 6) {
+            Inertia::flash([
+                'message' => 'Game is full',
             ]);
 
             return redirect('dashboard');
