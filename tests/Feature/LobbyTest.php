432a9236e7228bf9840de2ed0331355e9ecf3e75
Dom Elves <dom_elves@hotmail.co.uk>
2026-09-24 11:02:39 +0100

    re-add setting pw to blank string, test for game starting

--- tests/Feature/LobbyTest.php
+++ tests/Feature/LobbyTest.php
@@ -125,5 +125,9 @@ test('game will start if over two users are all ready', function () {
 
     Event::assertDispatched(GameCreated::class);
 
+    $this->assertDatabaseHas('games', [
+        'name' => $this->user . "'s game",
+        'code' => $joinCode,
+    ]);
     // i think i can only test the redirect signal in dusk or something
 });
